b023e31906a048e3dc3097239f63344148d7ea79
jongkiemodefischitRoblx <jongkiemodefischit@gmail.com>
2025-12-25 14:13:00 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+export const config = {
+  domain: 'https://anonpantatjebol.zonapanel.web.id',
+  plta: 'ptla_THnr1JBddVFUC4HvyJpOCJXaEw59Bw2mRJjG3l2I7o8',
+  pltc: 'ptlc_7DIJhnMV466e8JLH5MIARpCG5L6KfUKvy5zjn7Uef84',
+  loc: '1',
+  eggs: '15'
+};
